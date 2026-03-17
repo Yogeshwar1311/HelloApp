d088e8c0e2d3a3874d4f55a0aabe04c82ceb2516
Yogeshwar1311 <yogeshwarvs247@gmail.com>
2026-03-17 22:35:45 +0530

    coded uc5 to print hello world

---
+++ HelloApp5.java
@@ -0,0 +1,24 @@
+public class HelloApp5 {
+
+    public static void main(String[] args) {
+
+        // Default case
+        if (args.length == 0) {
+            System.out.println("Hello, World!");
+        } else {
+
+            StringBuilder names = new StringBuilder();
+
+            // Enhanced for loop
+            for (String name : args) {
+                names.append(name).append(", ");
+            }
+
+            // Remove last comma and space
+            names.setLength(names.length() - 2);
+
+            // Final output
+            System.out.println("Hello, " + names.toString() + "!");
+        }
+    }
+}
